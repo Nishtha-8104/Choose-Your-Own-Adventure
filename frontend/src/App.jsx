ac7fdb1e312bddf546afd86066a7af5931c2774c
Nishtha-8104 <patelnishtha78@gmail.com>
2026-02-23 20:22:02 +0530

    port chaged

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
-import StoryLoader from './components/storyLoader'  
+import StoryLoader from './components/StoryLoader'  
 import StoryGenerator from './components/StoryGenerator'
 
 function App() {
